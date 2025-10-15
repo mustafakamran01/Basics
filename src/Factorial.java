3dc99c8a0f55dfd15fb109b1cf75ecf5310c59b5
mustafakamran01 <kamran000mustafa@gmail.com>
2025-10-15 15:28:32 +0530

    Factorial program

---
+++ src/Factorial.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class Factorial {
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        // Taking input from user
+        System.out.println("Enter a number: ");
+        int num = scanner.nextInt();
+
+        int fact = 1;
+
+        // Checking if inout is greater than 0 or not
+        if(num <= 0){
+            System.out.println("Please give number more than O");
+        } else{
+            for(int i = num; i > 0; i--){
+                fact *= i;
+            }
+
+            System.out.println("Factorial: " + fact);
+        }
+    }
+}
